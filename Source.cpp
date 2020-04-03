b9a39de37ff3f8ed3189dda40e3afad0c20f6f8b
Gishan <gruw0001@student.monash.edu>
2020-04-03 18:32:40 +1100

    completed area and moment calculations with manual thresholding techniques. mmoi possibly incorrect.

--- Source.cpp
+++ Source.cpp
@@ -1,64 +1,139 @@
+////////////////////////////////////////////////////////////////
+// Skeleton program for TRC3500
+// Grabs images from a USB camera using OpenCV
+// Written by Andy Russell 09th February 2006
+// Modified by Michael Curtis 2011-2012 - updated for newer OpenCV
+/////////////////////////////////////////////////////////////////
 #include "cv.h"
 #include "highgui.h"
 #include <iostream>
 #include <stdio.h>
-#include "opencv2/opencv.hpp"
+//#include "opencv2/opencv.hpp"
 
-using namespace std;
+using namespace std; 
 using namespace cv;
 
-int main(int, char**)
+const double pi = 3.1415926535897;              //pi for use later
+
+////////////////////////////////////////////////////////////////
+// main - initialises OpenCV and captures an image and changes it
+////////////////////////////////////////////////////////////////
+int main()
 {
-    VideoCapture cap(0); // open the default camera
-    if (!cap.isOpened())  // check if we succeeded
-    {
-        cout << "camera failed" << endl;
+    CvCapture* capture = 0;
+    IplImage* processedImage = 0;
+
+    cout << "Group 2 Blob Statistics" << endl << "Press 'q' to quit" << endl;
+
+    // Initializes capturing video from camera
+    capture = cvCaptureFromCAM(0);
+    if (!capture) {
+        fprintf(stderr, "Could not initialize capturing...\n");
         return -1;
     }
-    
-    cout << "Group 2 Blob Statistics" << endl;
-
-    Mat video;
-    namedWindow("video", 1);
-    for (;;)
-    {
-        Mat frame;
-        //vector<Vec4i> hierarchy;
-        cap >> frame; // get a new frame from camera
-        flip(frame, frame, 1);
-        cvtColor(frame, video, COLOR_BGR2HSV);				//convert to hsv (opencv stores image in bgr not rgb)
-        GaussianBlur(video, video, Size(7, 7), 1.5, 1.5);		//smooth out the image, might be unnecessary becasue webcam quality is ok
-
-        //Mat video1, video2;
-        inRange(video, Scalar(80, 90, 90), Scalar(100, 240, 240), video);  //adjusted for blue
-
-        // These lines are needed if looking for red colour as the 'H' value wraps around
-        //inRange(video, Scalar(170, 150, 150), Scalar(180, 230, 230), video2);  //adjust for...
-        //Mat video = video1 | video2;
-        
-            //Clean up image
-            //erode(video, video, 3);
-            //dilate(video, video, 3);
-            morphologyEx(video, video, MORPH_OPEN, 11);			//adjust morph technique for best results	
-            morphologyEx(video, video, MORPH_CLOSE, 11);			//adjust morph technique for best results	
-             
-        Canny(video, video, 300, 3);        
-        //findContours(video, video, hierarchy, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
-
-        //Finding Centroid
-        Moments m = moments(video, true);					
-        Point p(m.m10 / m.m00, m.m01 / m.m00);
-        drawMarker(video, p, Scalar(128, 0, 0), MARKER_CROSS, 20, 1, 8);
 
 
- 
-        cout << "Centroid (x,y) = \t(" << m.m10 / m.m00 << ", " << m.m01 / m.m00 << ")" << endl;
-        imshow("video", video);
-        if (waitKey(16) >= 0) {
-            //cout << "inside waitkey" << endl;     //for debugging purposes
+    // Creates window
+    cvNamedWindow("Camera image", 1);
+
+    // Camera image
+    IplImage* frame = 0;
+
+
+    // Grabs and returns a frame from camera
+    frame = cvQueryFrame(capture);
+
+    // Print details of image
+    cout << "image width =" << frame->width << " height =" << frame->height;
+    cout << " depth =" << frame->depth << " channels =" << frame->nChannels << endl;
+
+    int height = frame->height;
+    int width = frame->width;
+    int m00 = 0;                   //m00
+    int m01 = 0;              //m01
+    int m10 = 0;              //m10
+    int centre_x = 0;
+    int centre_y = 0;
+    int m11 = 0;                    //m11 = m01 * m10
+    int m20 = 0;
+    int m02 = 0;
+    double mmoi = 0;                   //minimum moment of inertia
+
+    do {
+        // Grabs and returns a frame from camera
+        frame = cvQueryFrame(capture);
+        if (!frame) {
             break;
         }
-    }
-    // the camera will be deinitialized automatically in VideoCapture destructor
+
+        //reset values here
+        m00 = 0;       
+        m01 = 0;  
+        m10 = 0;  
+        m11 = 0;
+        mmoi = 0;
+        
+        //stepping through the pixels
+        for (int y = 0; y < frame->height; y++) {
+            for (int x = 0; x < frame->width; x++) {
+                // This is a pointer to the start of the current row.
+                //  Note: The image is stored as a 1-D array which is mapped back
+                //  into 2-space by multiplying the widthStep (the image width rounded to
+                //  a "nice" value, eg a multiple of 4 or 8 depending on the OS and CPU)
+                //  by the row number.
+                uchar* row = (uchar*)(frame->imageData + frame->widthStep * y);     //pointer to row?
+
+                int gray = (row[x * 3] + row[x * 3 + 1] + row[x * 3 + 2]) / 3;      // converting image to rgb by adding components and dividing by 3
+                
+                                                                                    
+                // Thresholding
+                if (gray > 250) {
+                    row[x * 3] = 255;      
+                    row[x * 3 + 1] = 255;
+                    row[x * 3 + 2] = 255;
+                    
+                    m00++;
+                    m01 = m01 + x;
+                    m10 = m10 + y;
+                    m11 = m01 * m10;
+                    m20 = m01 ^ 2;
+                    m02 = m10 ^ 2;
+
+                }
+
+                else {
+                    row[x * 3] = 0;
+                    row[x * 3 + 1] = 0;
+                    row[x * 3 + 2] = 0;
+                }
+
+            }
+        }
+
+        if (m00 != 0) {
+            centre_x = m01 / m00;
+            centre_y = m10 / m00;
+
+            mmoi = 0.5 * atan((2*(m00*m11 - m10*m01)/((m00*m20-m10^2)-(m00*m02-m01^2)))) * 180 / pi;
+
+            //Output lines
+            cout << "m00 (units)= \t" << m00;
+            cout << "\tCentre of m00 (x,y)= \t(" << centre_x << ", " << centre_y << ")";
+            cout << "\tMinimum Moment of Inertia = \t" << mmoi << endl;
+        }
+        
+        // Shows the resulting image in the window
+        cvShowImage("Camera image", frame);
+
+    } while ('q' != cvWaitKey(10));
+
+    //tidy up
+
+    // Releases the CvCapture structure
+    cvReleaseCapture(&capture);
+    // Destroys all the HighGUI windows
+    cvDestroyAllWindows();
+
     return 0;
-}
+
+} //end of main 
