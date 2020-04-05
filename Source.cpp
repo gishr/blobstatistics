fe9ecc920bdbb6c060829bccf6229bfc5ef840b8
Gishan <gruw0001@student.monash.edu>
2020-04-05 16:32:26 +1000

    added markers and more comments for code

--- Source.cpp
+++ Source.cpp
@@ -8,6 +8,7 @@
 #include "highgui.h"
 #include <iostream>
 #include <stdio.h>
+#include <math.h>
 //#include "opencv2/opencv.hpp"
 
 using namespace std; 
@@ -60,6 +61,9 @@ int main()
     double mmoi = 0;                   //minimum moment of inertia
     double num = 0;
     double den = 0;
+    double rad = 0;
+    double x2 = 0;
+    double y2 = 0;
 
     do {
         // Grabs and returns a frame from camera
@@ -89,13 +93,13 @@ int main()
                 
                                                                                     
                 // Thresholding
-                if (gray > 250) {
+                if (gray > 250) {                       //set light values to 255
                     row[x * 3] = 255;      
                     row[x * 3 + 1] = 255;
                     row[x * 3 + 2] = 255;
                     
-                    m00++;
-                    m01 = m01 + x;
+                    m00++;                              //increment area
+                    m01 = m01 + x;                      
                     m10 = m10 + y;
                     m11 = m01 * m10;
                     m20 = m01 * m01;
@@ -103,7 +107,7 @@ int main()
 
                 }
 
-                else {
+                else {                                  //set dark values to 0
                     row[x * 3] = 0;
                     row[x * 3 + 1] = 0;
                     row[x * 3 + 2] = 0;
@@ -113,37 +117,46 @@ int main()
         }
 
         if (m00 != 0) {
-            centre_x = m01 / m00;
-            centre_y = m10 / m00;
+            centre_x = m01 / m00;                       //centroid in x
+            centre_y = m10 / m00;                       //centroid in y
 
-            num = (2 * (m00 * m11 - m10 * m01));
-            den = (m00 * m20 - (m10 * m10)) - (m00 * m02 - (m01 * m01));
-            mmoi = 0.5 * atan(num / den) * 180 / pi;
+            num = (2 * (m00 * m11 - m10 * m01));                                //numerator
+            den = (m00 * m20 - (m10 * m10)) - (m00 * m02 - (m01 * m01));        //denominator
+            mmoi = 0.5 * atan(num / den) * 180 / CV_PI;                         //mmoi equation
             
             if (num >= 0) {
                 if (den >= 0) {
-                    cout << "Q1" << endl;           //  +/+
+                    //cout << "Q1" << endl;           //  +/+
                 }
                 else {
                     mmoi = 180 + mmoi ;
-                    cout << "Q2" << endl;           //  +/-
+                    //cout << "Q2" << endl;           //  +/-
                 }
             }
             else {
                 if (den >= 0) {
-                    cout << "Q3" << endl;           //  -/+
+                    //cout << "Q3" << endl;           //  -/+
                 }
                 else {
                     mmoi = 180 + mmoi;
-                    cout << "Q4" << endl;           //  -/-
+                    //cout << "Q4" << endl;           //  -/-
                 }
             }
-            
+            //Drawing Markers
+            rad = mmoi * CV_PI / 180;        
+            x2 = centre_x + cos(rad) * 100;
+            y2 = centre_y + sin(rad) * 100;
+
+            cvLine(frame, cvPoint(centre_x, 0), cvPoint(centre_x, 480), CV_RGB(255, 0, 0), 1, 8, 0);                //Vertical
+            cvLine(frame, cvPoint(0, centre_y), cvPoint(640, centre_y), CV_RGB(255, 0, 0), 1, 8, 0);                //Horizontal
+            cvLine(frame, cvPoint(centre_x, centre_y), cvPoint(int(x2), int(y2)), CV_RGB(0, 255, 0), 1, 8, 0);      // Orientation
 
             //Output lines
             cout << "m00 = \t" << m00;
-            cout << "\tCentre of area = \t" << centre_x << ", " << centre_y;
+            cout << "\tCentre of area =  " << centre_x << ", " << centre_y;
             cout << "\tMinimum Moment of Inertia = \t" << mmoi << endl;
+
+            
         }
         
         // Shows the resulting image in the window
